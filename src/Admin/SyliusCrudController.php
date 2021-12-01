a670cee04d2472917adf2503f98f3afe8fed0bb8
Arnaud RITTI <arnaud.ritti@adeliom.com>
2021-12-01 11:08:13 +0100

    fix admin crud

--- src/Admin/SyliusCrudController.php
+++ src/Admin/SyliusCrudController.php
@@ -9,8 +9,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class SyliusCrudController extends AbstractCrudController {
 
@@ -25,38 +27,52 @@ abstract class SyliusCrudController extends AbstractCrudController {
 
     public function createEditForm(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormInterface
     {
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityDto->getInstance()) , sprintf("sylius.%s.initialize_update", static::getResource()));
+        $initializeEvent = $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityDto->getInstance()) , sprintf("sylius.%s.initialize_update", static::getResource()));
+        $initializeEventResponse = $initializeEvent->getResponse();
+        if (null !== $initializeEventResponse) {
+            if($initializeEventResponse instanceof Response){
+                $initializeEventResponse->send();
+                exit;
+            }
+        }
         return parent::createEditForm($entityDto, $formOptions, $context);
     }
 
     public function createNewForm(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormInterface
     {
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityDto->getInstance()) , sprintf("sylius.%s.initialize_create", static::getResource()));
+        $initializeEvent = $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityDto->getInstance()) , sprintf("sylius.%s.initialize_create", static::getResource()));
+        $initializeEventResponse = $initializeEvent->getResponse();
+        if (null !== $initializeEventResponse) {
+            if($initializeEventResponse instanceof Response){
+                $initializeEventResponse->send();
+                exit;
+            }
+        }
         return parent::createNewForm($entityDto, $formOptions, $context);
     }
 
     public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
         $entityManager->persist($entityInstance);
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityInstance) , sprintf("sylius.%s.pre_update", static::getResource()));
+        $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityInstance) , sprintf("sylius.%s.pre_update", static::getResource()));
         $entityManager->flush();
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityInstance) , sprintf("sylius.%s.post_update", static::getResource()));
+        $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityInstance) , sprintf("sylius.%s.post_update", static::getResource()));
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityInstance) , sprintf("sylius.%s.pre_create", static::getResource()));
+        $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityInstance) , sprintf("sylius.%s.pre_create", static::getResource()));
         $entityManager->persist($entityInstance);
         $entityManager->flush();
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityInstance) , sprintf("sylius.%s.post_create", static::getResource()));
+        $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityInstance) , sprintf("sylius.%s.post_create", static::getResource()));
     }
 
     public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityInstance) , sprintf("sylius.%s.pre_delete", static::getResource()));
+        $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityInstance) , sprintf("sylius.%s.pre_delete", static::getResource()));
         $entityManager->remove($entityInstance);
         $entityManager->flush();
-        $this->get("event_dispatcher")->dispatch(new GenericEvent($entityInstance) , sprintf("sylius.%s.post_delete", static::getResource()));
+        $this->get("event_dispatcher")->dispatch(new ResourceControllerEvent($entityInstance) , sprintf("sylius.%s.post_delete", static::getResource()));
     }
 
 }
